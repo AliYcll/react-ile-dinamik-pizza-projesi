5ab659c077207fb7056efee9b38d4672db015f6c
Ali Yücel <ali.yucel675@hotmail.com>
2025-09-28 06:01:46 +0300

    Update pizza.cy.js

--- cypress/e2e/2-advanced-examples/pizza.cy.js
+++ cypress/e2e/2-advanced-examples/pizza.cy.js
@@ -1,46 +1,28 @@
 describe('Pizza Order Form', () => {
-  const testData = {
-    name: 'Ali Yücel',
-    size: 'M',
-    dough: 'İnce',
-    toppings: ['Pepperoni', 'Biber', 'Mısır', 'Kanada Jambonu'],
-    notes: 'Lütfen bol acılı olsun!'
-  };
-
-  const fillPizzaForm = (data, toppingLimit = data.toppings.length) => {
-    cy.get('.name-input').type(data.name);
-    cy.get(`.radio-group input[value="${data.size}"]`).parent('label').click();
-    cy.get('.dough-select').select(data.dough);
-
-    data.toppings.slice(0, toppingLimit).forEach(topping => {
-      cy.contains('.toppings-grid label', topping).find('input').check();
-    });
-
-    cy.get('.notes-input').type(data.notes);
-  };
-
   beforeEach(() => {
-    cy.visit('http://localhost:5173/order');
+    cy.visit('http://localhost:5173/order'); 
   });
 
-  it('should not enable submit if less than 4 toppings are selected', () => {
-    fillPizzaForm(testData, 2); // sadece 2 topping seçelim
-    cy.get('.btn-submit').should('be.disabled');
+  it('should enter text into the name input', () => {
+    cy.get('input.name-input').type('Ali Yücel').should('have.value', 'Ali Yücel');
   });
 
-  it('should fill the form correctly and submit', () => {
-    fillPizzaForm(testData); // tüm toppings (4 tane)
+  it('should select multiple toppings', () => {
+    cy.contains('label', 'Pepperoni').find('input').check();
+    cy.contains('label', 'Biber').find('input').check();
+    cy.contains('label', 'Mısır').find('input').check();
+    cy.contains('label', 'Sosis').find('input').check();
+  });
 
-    cy.get('.btn-submit').should('not.be.disabled').click();
-
+  it('should submit the form', () => {
+    cy.get('input.name-input').type('Ali Yücel');
+    cy.get('input[name="size"][value="M"]').parent('label').click();
+    cy.get('select.dough-select').select('İnce');
+    cy.contains('label', 'Pepperoni').find('input').check();
+    cy.contains('label', 'Biber').find('input').check();
+    cy.contains('label', 'Mısır').find('input').check();
+    cy.contains('label', 'Sosis').find('input').check();
+    cy.get('button[type="submit"]').click();
     cy.url().should('include', '/success');
-    cy.get('.success-title').should('contain', 'SİPARİŞ ALINDI');
-
-    cy.contains(`Boyut: ${testData.size}`).should('exist');
-    cy.contains(`Hamur: ${testData.dough}`).should('exist');
-
-    testData.toppings.forEach(topping => {
-      cy.contains(topping).should('exist');
-    });
   });
 });
